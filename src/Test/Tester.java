da182e7dac83d78fc7ce7328d1f301196b067327
Thorey Jonsdottir <thorey.jonsdottir@gmail.com>
2018-02-14 14:09:58 +0100

    Thorey

--- src/Test/Tester.java
+++ src/Test/Tester.java
@@ -16,5 +16,6 @@ public class Tester
         System.out.println("Tester dette.");
         System.out.println("brian");
         System.out.println("Borgar!!!");
+        System.out.println("Thorey");
     }
 }
