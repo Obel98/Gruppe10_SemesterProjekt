380aa6f4cb23ee9f719b071809f1bd94058c5824
Rasmus Hansen <rasmush92@live.com>
2018-04-27 10:58:11 +0200

    Bosted, Kommune Getters & Setters

--- src/gruppe10_semesterprojekt/Kommune.java
+++ src/gruppe10_semesterprojekt/Kommune.java
@@ -6,5 +6,44 @@ public class Kommune {
     private String address;
     private String phoneNumber;
     private String email;
-    
-}
+
+        public Kommune(String name, String address, String phoneNumber, String email){
+            this.name = name;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+
