a3717f82e5b6efc60051d41cdb66a01edafd2316
Rasmus Hansen <rasmush92@live.com>
2018-04-27 10:30:06 +0200

    Borger code implemented 10%

--- src/gruppe10_semesterprojekt/Borger.java
+++ src/gruppe10_semesterprojekt/Borger.java
@@ -1,12 +1,96 @@
 package gruppe10_semesterprojekt;
 
 public class Borger {
-    
-    private String name;
-    private String address;
-    private String email;
-    private String phoneNumber;
-    private String cpr;
-    
-    
+
+  private String name;
+  private String lastName;
+  private String address;
+  private String phoneNumber;
+  private String email;
+  private case Sag;
+  private contact String;
+  private history ArrayList<Sag>;
+  private password String;
+  private cpr String;
+
+  public Borger(String cpr, String name, String lastName, String address, String phoneNumber, String email){
+      this.cpr = cpr;
+      this.name = name;
+      this.lastName = lastName;
+      this.address = address;
+      this.phoneNumber = phoneNumber;
+      this.email = email;
+
+      this.password = generatePassword();
+
+
+      this.case = Sag();
+      this.contact = contact;
+      this.history = ArrayList<Sag>;
+
+  }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public java.lang.String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(java.lang.String lastName) {
+        this.lastName = lastName;
+    }
+
+    public java.lang.String getAddress() {
+        return address;
+    }
+
+    public void setAddress(java.lang.String address) {
+        this.address = address;
+    }
+
+    public java.lang.String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(java.lang.String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public java.lang.String getEmail() {
+        return email;
+    }
+
+    public void setEmail(java.lang.String email) {
+        this.email = email;
+    }
+
+    public contact getString() {
+        return String;
+    }
+
+    public void setString(cpr string) {
+        String = string;
+    }
+
+    public void setString(password string) {
+        String = string;
+    }
+
+    public void setString(contact string) {
+        String = string;
+    }
+
+    public history getArrayList() {
+        return ArrayList;
+    }
+
+    public void setArrayList(history arrayList) {
+        ArrayList = arrayList;
+    }
 }
