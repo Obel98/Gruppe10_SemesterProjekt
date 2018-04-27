624a437ca60a56e1539b78b01b95dbe87d456bfd
Rasmus Hansen <rasmush92@live.com>
2018-04-27 10:10:23 +0200

    Reformat

--- src/gruppe10_semesterprojekt/Sagsbehandler.java
+++ src/gruppe10_semesterprojekt/Sagsbehandler.java
@@ -2,15 +2,14 @@ package gruppe10_semesterprojekt;
 
 public class Sagsbehandler {
 
-    
+
     private String name;
     private String id;
     private String password;
     private String phoneNumber;
     private String email;
 
-    public Sagsbehandler(String name, String phoneNumber, String email)
-    {
+    public Sagsbehandler(String name, String phoneNumber, String email) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
@@ -18,60 +17,49 @@ public class Sagsbehandler {
         this.password = generatePassword();
     }
 
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
-    
-    public String getId()
-    {
+
+    public String getId() {
         return id;
     }
-    
-    private String generateId(String name)
-    {
+
+    private String generateId(String name) {
         String generatedId = name;
         return generatedId;
     }
 
-    public String getPassword()
-    {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password)
-    {
+    public void setPassword(String password) {
         this.password = password;
     }
-    
-    private String generatePassword()
-    {
+
+    private String generatePassword() {
         String generatedPassword = "superSecretPassword";
         return generatedPassword;
     }
 
-    public String getPhoneNumber()
-    {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber)
-    {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail(String email) {
         this.email = email;
     }
 
