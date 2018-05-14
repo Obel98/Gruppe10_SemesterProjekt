b3c7f3f3e8bf7dcf849d1e138dae877d58e12272
Rasmus Hansen <rasmush92@live.com>
2018-05-14 23:45:35 +0200

    Small changes

--- src/Presentation/ResetPasswordController.java
+++ src/Presentation/ResetPasswordController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -47,6 +48,8 @@ public class ResetPasswordController implements Initializable {
 
     @FXML
     private void backOnAction(ActionEvent event) {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
