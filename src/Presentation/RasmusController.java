32a0a6c73770d314eba019b521e3c034918ea76d
Borgar Bordoy <bobor16@student.sdu.dk>
2018-05-24 15:09:17 +0200

    Rasmus <3

---
+++ src/Presentation/RasmusController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
+
+/**
+ * FXML Controller class
+ *
+ * @author borga
+ */
+public class RasmusController implements Initializable {
+
+    @FXML
+    private WebView webView;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
